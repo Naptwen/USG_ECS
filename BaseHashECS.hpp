3bf482f15c4bdcab094d9d9be1bb420efe381feb
Mr BIV <47798805+Naptwen@users.noreply.github.com>
2023-04-23 15:48:17 +0900

    Update BaseHashECS.hpp

--- BaseHashECS.hpp
+++ BaseHashECS.hpp
@@ -3,6 +3,7 @@
 //##############################################
 #ifndef _BASE_HASH_TABLE_H_
 #define _BASE_HASH_TABLE_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory>
@@ -10,30 +11,30 @@
 #include <utility>
 #include <unordered_map>
 #include <iostream>
+#if __cplusplus >= 201703L
+
+#include <any>
 #include <typeindex>
 
-#if __cplusplus >= 201703L
-#include <any>
-
 using _usg_componets = std::unordered_map<std::type_index, std::any>;
-using _usg_buckets = std::unordered_map<std::string_view,_usg_componets>;
+using _usg_buckets = std::unordered_map<std::string, _usg_componets>;
 #define _usg_any_cast(_TYPE, KEY) std::any_cast<_TYPE&>(bucket_list.at(KEY).at(typeid(_TYPE)))
 
 class HASH_TABLE{
     _usg_buckets bucket_list;
     template<typename _T1> 
-    void _Add(const std::string_view key, _T1& _obj1){
+    void _Add(const std::string& key, _T1& _obj1){
         bucket_list[key][typeid(_T1)] =_obj1;
     }
     
     template<typename _T1, typename ...Args> 
-    void _Add(const std::string_view key, _T1& _obj1, Args& ...args){
+    void _Add(const std::string& key, _T1& _obj1, Args& ...args){
         bucket_list[key][typeid(_T1)] = _obj1;
         _Add<Args...>(key, args...);
     }
     
     template<class _Tuple, size_t N, typename _T1> 
-    void _Filter(const std::string_view key, size_t& n, _Tuple& tup){
+    void _Filter(const std::string& key, size_t& n, _Tuple& tup){
         if constexpr (std::is_pointer_v<_T1>){
             using _Trp = std::remove_pointer_t<_T1>;
             if(bucket_list.at(key).count(typeid(_Trp)) && ++n){
@@ -46,7 +47,7 @@ class HASH_TABLE{
     }
     
     template<class _Tuple, size_t N, typename _T1, typename _T2, typename ...Args>
-    void _Filter(const std::string_view key, size_t& n, _Tuple& tup){
+    void _Filter(const std::string& key, size_t& n, _Tuple& tup){
         if constexpr (std::is_pointer_v<_T1>){
             using _Trp = std::remove_pointer_t<_T1>;
             if(bucket_list.at(key).count(typeid(_Trp)) && ++n){
@@ -62,16 +63,16 @@ class HASH_TABLE{
 public:
 
     template<typename... Args>
-    void Add(const std::string_view key, Args... args){
+    void Add(const std::string& key, Args... args){
         _Add<Args...>(key, args...);
     }
 
-    void Delete(const std::string_view key){
+    void Delete(const std::string& key){
         bucket_list.erase(key);
     }
 
     template<typename T>
-    T* Get(const std::string_view key){
+    T* Get(const std::string& key){
         assert(bucket_list.count(key));
         assert(bucket_list.at(key).count(typeid(T)));
         return &std::any_cast<T>(bucket_list.at(key).at(typeid(T)));
@@ -85,6 +86,17 @@ public:
             _Filter<std::tuple<Args...>, 0, Args...>(temp.first, check, tup);
             if(check == sizeof...(Args))
                 std::apply(func, tup);
+        }
+    }
+
+    void View(){
+        std::cout << bucket_list.size() << std::endl;
+        for(auto& temp : bucket_list){
+            std::cout << temp.first << " has "; 
+            for(auto& tmp : temp.second){
+                std::cout << tmp.first.name() << std::endl;
+            }
+            std::cout << std::endl;
         }
     }
 };
