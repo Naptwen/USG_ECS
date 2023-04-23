bb9b89de30a1956b9c3891479243c4b704b3e4a9
Mr BIV <47798805+Naptwen@users.noreply.github.com>
2023-04-23 14:52:39 +0900

    Update BaseHashECS.hpp

--- BaseHashECS.hpp
+++ BaseHashECS.hpp
@@ -1,7 +1,6 @@
-#################################################
-##  GNU Affero 3.0 License Useop Gim 2023-04-23 #
-#################################################
-
+//##############################################
+//## GNU Affero 3.0 Useop Gim April 23, 2023. ##
+//##############################################
 #ifndef _BASE_HASH_TABLE_H_
 #define _BASE_HASH_TABLE_H_
 #include <stdio.h>
@@ -17,7 +16,7 @@
 
 using _usg_componets = std::unordered_map<std::type_index, std::any>;
 using _usg_buckets = std::unordered_map<std::string_view,_usg_componets>;
-#define _usg_any_cast(_TYPE, KEY) std::any_cast<_TYPE>(bucket_list.at(KEY).at(typeid(std::remove_reference_t<_TYPE>)))
+#define _usg_any_cast(_TYPE, KEY) std::any_cast<_TYPE&>(bucket_list.at(KEY).at(typeid(_TYPE)))
 
 class HASH_TABLE{
     _usg_buckets bucket_list;
@@ -34,17 +33,29 @@ class HASH_TABLE{
     
     template<class _Tuple, size_t N, typename _T1> 
     void _Filter(const std::string_view key, size_t& n, _Tuple& tup){
-        if(bucket_list.at(key).count(typeid(_T1)) && ++n){
+        if constexpr (std::is_pointer_v<_T1>){
+            using _Trp = std::remove_pointer_t<_T1>;
+            if(bucket_list.at(key).count(typeid(_Trp)) && ++n){
+                std::get<N>(tup) = &_usg_any_cast(_Trp, key);
+            }
+        }
+        else if(bucket_list.at(key).count(typeid(_T1)) && ++n){
             std::get<N>(tup) = _usg_any_cast(_T1, key);
         }         
     }
     
     template<class _Tuple, size_t N, typename _T1, typename _T2, typename ...Args>
     void _Filter(const std::string_view key, size_t& n, _Tuple& tup){
-        if(bucket_list.at(key).count(typeid(_T1)) && ++n){
+        if constexpr (std::is_pointer_v<_T1>){
+            using _Trp = std::remove_pointer_t<_T1>;
+            if(bucket_list.at(key).count(typeid(_Trp)) && ++n){
+                std::get<N>(tup) = &_usg_any_cast(_Trp, key);
+            }
+        }
+        else if(bucket_list.at(key).count(typeid(_T1)) && ++n){
             std::get<N>(tup) = _usg_any_cast(_T1, key);
-            _Filter<_Tuple, N + 1, _T2, Args...>(key, n, tup);
-        }        
+        }
+        _Filter<_Tuple, N + 1, _T2, Args...>(key, n, tup);
     }
 
 public:
